32483868f5761fa5cfa38187af7e1599bf5c6f85
Nitesh-raghu45 <niteshraghuwanshi68@gmail.com>
2026-02-04 19:00:06 +0530

    this commit comes after the  embedding

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="medical_chatbot",
+    name="clinicalrag",
     version="0.1.0",
     author="Nitesh Raghuwanshi",
     author_email="niteshraghuwanshi68@gmail.com",
+    description="Evidence-grounded clinical RAG system for medical QA",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=[]
+    python_requires=">=3.9",
+    install_requires=[],
 )
